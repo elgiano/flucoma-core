1d6c56021a99a95b0a8614c58fc7a74a42df34c1
elgiano <elgiano@gmail.com>
2022-09-05 12:20:35 +0200

    AmpFeature: select diff, fast, slow

--- include/clients/rt/AmpFeatureClient.hpp
+++ include/clients/rt/AmpFeatureClient.hpp
@@ -25,6 +25,7 @@ namespace client {
 namespace ampfeature {
 
 enum AmpFeatureParamIndex {
+  kSelect,
   kFastRampUpTime,
   kFastRampDownTime,
   kSlowRampUpTime,
@@ -34,6 +35,7 @@ enum AmpFeatureParamIndex {
 };
 
 constexpr auto AmpFeatureParams = defineParameters(
+    ChoicesParam("select","Selection of Outputs","diff","fast","slow"),
     LongParam("fastRampUp", "Fast Envelope Ramp Up Length", 1, Min(1)),
     LongParam("fastRampDown", "Fast Envelope Ramp Down Length", 1, Min(1)),
     LongParam("slowRampUp", "Slow Envelope Ramp Up Length", 100, Min(1)),
@@ -43,6 +45,8 @@ constexpr auto AmpFeatureParams = defineParameters(
 
 class AmpFeatureClient : public FluidBaseClient, public AudioIn, public AudioOut
 {
+
+  static constexpr index mMaxFeatures = 3;
 
 public:
   using ParamDescType = decltype(AmpFeatureParams);
@@ -63,7 +67,7 @@ public:
   AmpFeatureClient(ParamSetViewType& p) : mParams(p)
   {
     audioChannelsIn(1);
-    audioChannelsOut(1);
+    audioChannelsOut(mMaxFeatures);
     FluidBaseClient::setInputLabels({"audio input"});
     FluidBaseClient::setOutputLabels({"1 when slice detected, 0 otherwise"});
   }
@@ -82,12 +86,25 @@ public:
       mAlgorithm.init(get<kSilenceThreshold>(), hiPassFreq);
     }
     
+    auto selection = get<kSelect>();
+    index numSelected = asSigned(selection.count());
+    index numOuts = std::min<index>(mMaxFeatures,numSelected);
+    audioChannelsOut(numOuts);
+
     for (index i = 0; i < input[0].size(); i++)
     {
-      output[0](i) = static_cast<T>(mAlgorithm.processSample(
+      double fast, slow;
+      std::tie(fast, slow) = mAlgorithm.processSampleSeparate(
           input[0](i), get<kSilenceThreshold>(), get<kFastRampUpTime>(),
           get<kSlowRampUpTime>(), get<kFastRampDownTime>(),
-          get<kSlowRampDownTime>(), hiPassFreq));
+          get<kSlowRampDownTime>(), hiPassFreq);
+      index j = 0;
+      if (selection[0])
+        output[j++](i) = static_cast<T>(fast-slow);
+      if (selection[1])
+        output[j++](i) = static_cast<T>(fast);
+      if (selection[2])
+        output[j++](i) = static_cast<T>(slow);
     }
   }
   index latency() { return 0; }
@@ -101,6 +118,60 @@ public:
 private:
   algorithm::Envelope mAlgorithm;
 };
+
+template <typename HostMatrix, typename HostVectorView>
+struct NRTAmpFeature
+{
+  template <typename Client, typename InputList, typename OutputList>
+  static Result process(Client& client, InputList& inputBuffers,
+                        OutputList& outputBuffers, index nFrames, index nChans,
+                        std::pair<index, index> userPadding, FluidContext& c)
+  {
+    // expecting a single buffer in and a single buffer out
+    assert(inputBuffers.size() == 1);
+    assert(outputBuffers.size() == 1);
+
+    index maxOuts = client.audioChannelsOut();
+
+    HostMatrix inputData(nChans, nFrames);
+    HostMatrix outputData(maxOuts * nChans, nFrames);
+
+    // ignoring userPadding as AmpFeature has no padding options and no latency
+
+    double sampleRate{0};
+
+    for (index i = 0; i < nChans; ++i)
+    {
+      BufferAdaptor::ReadAccess thisInput(inputBuffers[0].buffer);
+      sampleRate = thisInput.sampleRate();
+      inputData.row(i)(Slice(0, nFrames)) <<=
+            thisInput.samps(inputBuffers[0].startFrame, nFrames,
+                            inputBuffers[0].startChan + i);
+      std::vector<HostVectorView> input{inputData.row(i)};
+
+      std::vector<HostVectorView> outputs;
+      outputs.reserve(maxOuts);
+      for(index j = 0; j < maxOuts; ++j)
+        outputs.emplace_back(outputData.row(i * maxOuts + j));
+
+      client.reset();
+      client.process(input, outputs, c);
+    }
+
+
+    BufferAdaptor::Access thisOutput(outputBuffers[0]);
+    index numOuts = client.audioChannelsOut();
+    Result                r = thisOutput.resize(nFrames, nChans * numOuts, sampleRate);
+    if (!r.ok()) return r;
+    for (index j = 0; j < nChans; ++j) {
+      for (index k = 0; k < numOuts; ++k)
+      thisOutput.samps(j * numOuts + k) <<=
+        outputData.row(j * maxOuts + k)(Slice(0, nFrames));
+    }
+
+    return {};
+  }
+};
 } // namespace ampfeature
 
 using RTAmpFeatureClient = ClientWrapper<ampfeature::AmpFeatureClient>;
@@ -110,7 +181,7 @@ auto constexpr NRTAmpFeatureParams = makeNRTParams<ampfeature::AmpFeatureClient>
     BufferParam("features", "Feature Buffer"));
 
 using NRTAmpFeatureClient =
-    NRTStreamAdaptor<ampfeature::AmpFeatureClient, decltype(NRTAmpFeatureParams),
+    impl::NRTClientWrapper<ampfeature::NRTAmpFeature, ampfeature::AmpFeatureClient, decltype(NRTAmpFeatureParams),
                     NRTAmpFeatureParams, 1, 1>;
 
 using NRTThreadedAmpFeatureClient = NRTThreadingAdaptor<NRTAmpFeatureClient>;
